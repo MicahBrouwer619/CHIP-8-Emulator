55d63ad234fd65c5920263a089dfd29c9dbc53e2
MicahBrouwer619 <flyingdutchmanbrouwer@gmail.com>
2025-06-02 15:25:04 -0700

    Create Emulated-Instructions.cpp

---
+++ src/Emulated-Instructions.cpp
@@ -0,0 +1,18 @@
+//clear the routine
+void Chip8::OP_00E0()
+{
+	memset(video, 0, sizeof(video));
+}
+//return from a subroutine
+void Chip8::OP_00EE()
+{
+	--sp;
+	pc = stack[sp];
+}
+//jump to location
+void Chip8::OP_1nnn()
+{
+	uint16_t address = opcode & 0x0FFFu;
+
+	pc = address;
+}
