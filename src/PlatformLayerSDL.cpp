e28cd97bf757771e62d356ffae59271a9a44f9a7
MicahBrouwer619 <flyingdutchmanbrouwer@gmail.com>
2025-06-30 15:33:10 +0000

    fully fixed plat layer

--- src/PlatformLayerSDL.cpp
+++ src/PlatformLayerSDL.cpp
@@ -1,227 +1,86 @@
 #include "Platform.hpp"
 #include <SDL3/SDL.h>
+#include <iostream>
 
 Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
 
-	window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow(title, windowWidth, windowHeight, 0);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
 
-	texture = SDL_CreateTexture(
-		renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+    texture = SDL_CreateTexture(
+        renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+    if (!texture) {
+        std::cerr << "SDL_CreateTexture Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
 }
 
 Platform::~Platform()
 {
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+    if (texture) SDL_DestroyTexture(texture);
+    if (renderer) SDL_DestroyRenderer(renderer);
+    if (window) SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 void Platform::Update(void const* buffer, int pitch)
 {
-	SDL_UpdateTexture(texture, nullptr, buffer, pitch);
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-	SDL_RenderPresent(renderer);
+    SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
 
 bool Platform::ProcessInput(uint8_t* keys)
 {
-	bool quit = false;
+    bool quit = false;
+    SDL_Event event;
 
-	SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_EVENT_QUIT:
+                quit = true;
+                break;
 
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-			case SDL_QUIT:
-			{
-				quit = true;
-			} break;
-
-			case SDL_KEYDOWN:
-			{
-				switch (event.key.keysym.sym)
-				{
-					case SDLK_ESCAPE:
-					{
-						quit = true;
-					} break;
-
-					case SDLK_x:
-					{
-						keys[0] = 1;
-					} break;
-
-					case SDLK_1:
-					{
-						keys[1] = 1;
-					} break;
-
-					case SDLK_2:
-					{
-						keys[2] = 1;
-					} break;
-
-					case SDLK_3:
-					{
-						keys[3] = 1;
-					} break;
-
-					case SDLK_q:
-					{
-						keys[4] = 1;
-					} break;
-
-					case SDLK_w:
-					{
-						keys[5] = 1;
-					} break;
-
-					case SDLK_e:
-					{
-						keys[6] = 1;
-					} break;
-
-					case SDLK_a:
-					{
-						keys[7] = 1;
-					} break;
-
-					case SDLK_s:
-					{
-						keys[8] = 1;
-					} break;
-
-					case SDLK_d:
-					{
-						keys[9] = 1;
-					} break;
-
-					case SDLK_z:
-					{
-						keys[0xA] = 1;
-					} break;
-
-					case SDLK_c:
-					{
-						keys[0xB] = 1;
-					} break;
-
-					case SDLK_4:
-					{
-						keys[0xC] = 1;
-					} break;
-
-					case SDLK_r:
-					{
-						keys[0xD] = 1;
-					} break;
-
-					case SDLK_f:
-					{
-						keys[0xE] = 1;
-					} break;
-
-					case SDLK_v:
-					{
-						keys[0xF] = 1;
-					} break;
-				}
-			} break;
-
-			case SDL_KEYUP:
-			{
-				switch (event.key.keysym.sym)
-				{
-					case SDLK_x:
-					{
-						keys[0] = 0;
-					} break;
-
-					case SDLK_1:
-					{
-						keys[1] = 0;
-					} break;
-
-					case SDLK_2:
-					{
-						keys[2] = 0;
-					} break;
-
-					case SDLK_3:
-					{
-						keys[3] = 0;
-					} break;
-
-					case SDLK_q:
-					{
-						keys[4] = 0;
-					} break;
-
-					case SDLK_w:
-					{
-						keys[5] = 0;
-					} break;
-
-					case SDLK_e:
-					{
-						keys[6] = 0;
-					} break;
-
-					case SDLK_a:
-					{
-						keys[7] = 0;
-					} break;
-
-					case SDLK_s:
-					{
-						keys[8] = 0;
-					} break;
-
-					case SDLK_d:
-					{
-						keys[9] = 0;
-					} break;
-
-					case SDLK_z:
-					{
-						keys[0xA] = 0;
-					} break;
-
-					case SDLK_c:
-					{
-						keys[0xB] = 0;
-					} break;
-
-					case SDLK_4:
-					{
-						keys[0xC] = 0;
-					} break;
-
-					case SDLK_r:
-					{
-						keys[0xD] = 0;
-					} break;
-
-					case SDLK_f:
-					{
-						keys[0xE] = 0;
-					} break;
-
-					case SDLK_v:
-					{
-						keys[0xF] = 0;
-					} break;
-				}
-			} break;
-		}
-	}
-
-	return quit;
+            case SDL_EVENT_KEY_DOWN:
+            case SDL_EVENT_KEY_UP: {
+                bool pressed = (event.type == SDL_EVENT_KEY_DOWN);
+                switch (event.key.key) {
+                    case SDL_SCANCODE_ESCAPE: quit = pressed; break;
+                    case SDL_SCANCODE_X: keys[0] = pressed; break;
+                    case SDL_SCANCODE_1: keys[1] = pressed; break;
+                    case SDL_SCANCODE_2: keys[2] = pressed; break;
+                    case SDL_SCANCODE_3: keys[3] = pressed; break;
+                    case SDL_SCANCODE_Q: keys[4] = pressed; break;
+                    case SDL_SCANCODE_W: keys[5] = pressed; break;
+                    case SDL_SCANCODE_E: keys[6] = pressed; break;
+                    case SDL_SCANCODE_A: keys[7] = pressed; break;
+                    case SDL_SCANCODE_S: keys[8] = pressed; break;
+                    case SDL_SCANCODE_D: keys[9] = pressed; break;
+                    case SDL_SCANCODE_Z: keys[0xA] = pressed; break;
+                    case SDL_SCANCODE_C: keys[0xB] = pressed; break;
+                    case SDL_SCANCODE_4: keys[0xC] = pressed; break;
+                    case SDL_SCANCODE_R: keys[0xD] = pressed; break;
+                    case SDL_SCANCODE_F: keys[0xE] = pressed; break;
+                    case SDL_SCANCODE_V: keys[0xF] = pressed; break;
+                }
+                break;
+            }
+        }
+    }
+    return quit;
 }
