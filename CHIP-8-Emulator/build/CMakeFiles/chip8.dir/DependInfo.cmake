1f4aaf4c2ae28e17f061cb302c9b048f909e9ae8
MicahBrouwer619 <flyingdutchmanbrouwer@gmail.com>
2025-07-09 15:59:22 +0000

    restructured directories

---
+++ CHIP-8-Emulator/build/CMakeFiles/chip8.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/src/Chip8.cpp" "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/build/CMakeFiles/chip8.dir/src/Chip8.cpp.o"
+  "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/src/Chip8ClassStructure.cpp" "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/build/CMakeFiles/chip8.dir/src/Chip8ClassStructure.cpp.o"
+  "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/src/Platform.cpp" "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/build/CMakeFiles/chip8.dir/src/Platform.cpp.o"
+  "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/src/PlatformLayerSDL.cpp" "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/build/CMakeFiles/chip8.dir/src/PlatformLayerSDL.cpp.o"
+  "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/src/main.cpp" "/workspaces/CHIP-8-Emulator/CHIP-8-Emulator/build/CMakeFiles/chip8.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
